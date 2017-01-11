fdf9d231bd5a6c831488315ce78be08b5d24fec2
gkreder <gkreder@gmail.com>
2017-01-11 12:07:35 -0800

    Fixed bubble and quick sort and changed travis link in README

--- example/algs.py
+++ example/algs.py
@@ -7,19 +7,37 @@ def pointless_sort(x):
     """
     return np.array([1,2,3])
 
-def bubblesort(x):
+def bubblesort(A):
     """
     Describe how you are sorting `x`
+
+    from http://www.geekviewpoint.com/python/sorting/bubblesort
     """
+    def swap( A, x, y ):
+      tmp = A[x]
+      A[x] = A[y]
+      A[y] = tmp
 
-    assert 1 == 2
-    return x
+    for i in range(len(A)):
+        for k in range( len( A ) - 1, i, -1 ):
+            if ( A[k] < A[k - 1] ):
+                swap( A, k, k - 1 )
+ 
+    return A
 
-def quicksort(x):
+def quicksort(arr):
     """
     Describe how you are sorting `x`
+
+    from http://stackoverflow.com/questions/18262306/quicksort-with-python
     """
 
-    assert 1 == 2
-    return
+    if len(arr) == 0:
+        return []
+
+    return quicksort([x for x in arr if x < arr[0]]) \
+        + [x for x in arr if x == arr[0]] \
+        + quicksort([x for x in arr if x > arr[0]])
+    
+    return arr
 
